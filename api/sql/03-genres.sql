c9514c64d8b00c7c89c3fba7469beeaad19a6c5f
RomainGuarinoni <romain.guar91@gmail.com>
2022-01-19 23:58:12 +0100

    feat(sql): add value to genre.sql and intrument.sql

--- api/sql/03-genres.sql
+++ api/sql/03-genres.sql
@@ -20,12 +20,33 @@ INSERT INTO genres VALUES (
     ), (
         '54d47edb-97e5-4f45-b2a1-f3048b038650',
         'Jazz'
-);
+    ), (
+        'c710920b-b062-4223-adef-bfb15e344ec2',
+        'Electro'
+    ), (
+        '0c1e7d1c-e612-4b13-961b-c41854053ac1',
+        'Pop'
+    ), (
+        '953711c4-52ac-42ca-adc7-9f4734de3f88',
+        'Reggae'
+    );
 
 INSERT INTO musicians_genres VALUES (
         '8f6c1dd5-7444-46c9-b673-840731bfd041',
         '54d47edb-97e5-4f45-b2a1-f3048b038650'
-    ),(
+    ), (
         '8f6c1dd5-7444-46c9-b673-840731bfd041',
         '7d68d33c-3eff-4f5e-985b-c7d9e058e23a'
+    ), (
+        '8c9a685a-2be9-4cf0-a03c-0b316fc4b515',
+        '0c1e7d1c-e612-4b13-961b-c41854053ac1'
+    ), (
+        '8c9a685a-2be9-4cf0-a03c-0b316fc4b515',
+        '953711c4-52ac-42ca-adc7-9f4734de3f88'
+    ), (
+        '74f3d613-39e7-4891-8491-8114988f490c',
+        'c710920b-b062-4223-adef-bfb15e344ec2'
+    ), (
+        '74f3d613-39e7-4891-8491-8114988f490c',
+        '54d47edb-97e5-4f45-b2a1-f3048b038650'
     );
--- api/sql/04-instruments.sql
+++ api/sql/04-instruments.sql
@@ -19,6 +19,9 @@ INSERT INTO instruments VALUES (
 ) , (
     'e345114e-7723-42eb-8ed1-f26cd2f9d084',
     'guitar'
+), (
+    'e1080fc2-fee5-4266-be45-78e1b8ac78c3',
+    'piano'
 );
 
 INSERT INTO musicians_instruments VALUES (
@@ -26,5 +29,17 @@ INSERT INTO musicians_instruments VALUES (
     'cd836a31-1663-4a11-8a88-0a249aa70793'
 ), (
     '8f6c1dd5-7444-46c9-b673-840731bfd041',
+    'e345114e-7723-42eb-8ed1-f26cd2f9d084'
+), (
+    '8c9a685a-2be9-4cf0-a03c-0b316fc4b515',
+    'e1080fc2-fee5-4266-be45-78e1b8ac78c3'
+), (
+    '8f6c1dd5-7444-46c9-b673-840731bfd041',
+    'e1080fc2-fee5-4266-be45-78e1b8ac78c3'
+), (
+    '74f3d613-39e7-4891-8491-8114988f490c',
+    'e1080fc2-fee5-4266-be45-78e1b8ac78c3'
+), (
+    '74f3d613-39e7-4891-8491-8114988f490c',
     'e345114e-7723-42eb-8ed1-f26cd2f9d084'
 );
