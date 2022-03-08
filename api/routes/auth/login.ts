673c56f88033e6c0435e57f199fb388a0dfce2cb
RomainGuarinoni <romain.guar91@gmail.com>
2022-03-08 19:06:46 +0100

    feat(route): update post login with typeorm

--- api/routes/auth/login.ts
+++ api/routes/auth/login.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
-import sql from 'sql-template-strings';
-import pg from '../../postgres';
-import { v4 as uuidV4 } from 'uuid';
 import generateToken, { GrantTypes } from '../../auth/generateToken';
 import type { operations } from '@schema';
 import type { Request } from 'express';
 import type core from 'express-serve-static-core';
 import type { getHTTPCode, getRequestBody, getResponsesBody } from '@typing';
+import { getRepository } from 'typeorm';
+import { Musician, Token } from '../../entity';
 
 type Login = operations['login'];
 
@@ -21,19 +20,16 @@ router.post(
   ) => {
     const body = req.body;
 
-    const { rows } = await pg.query(
-      sql`SELECT id, password 
-            FROM musicians 
-            WHERE email=${body.email} 
-        `,
-    );
+    const musician = await getRepository(Musician).findOne({
+      email: body.email,
+    });
 
-    if (rows.length === 0) {
+    if (!musician) {
       return res.status(400).json({ msg: 'E_UNFOUND_USER' });
     }
 
-    const password = rows[0].password;
-    const userId = rows[0].id;
+    const password = musician.password;
+    const userId = musician.id;
 
     try {
       const result = await bcrypt.compare(body.password, password);
@@ -46,75 +42,24 @@ router.post(
 
       const refreshToken = generateToken(GrantTypes.RefreshToken, userId);
 
-      await pg.query(sql`
-        INSERT INTO tokens (
-          id,
-          token,
-          musician
-        ) VALUES (
-          ${uuidV4()},
-          ${refreshToken},
-          ${userId}
-        )
-      `);
+      const token = new Token();
+      token.token = refreshToken;
+      token.grandType = GrantTypes.RefreshToken;
+      token.musician = musician;
 
-      res.cookie('refreshToken', refreshToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'strict',
+      await getRepository(Token).save(token);
+
+      const musicianInfo = await getRepository(Musician).findOne({
+        where: { id: userId },
+        relations: ['instruments', 'genres'],
       });
-
-      res.cookie('accessToken', accessToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'strict',
-        maxAge: 60,
-      });
-
-      const {
-        rows: [musicianResponse],
-      } = await pg.query(sql`
-            SELECT 
-            id,
-            email,
-            given_name as "givenName",
-            family_name as "familyName",
-            phone,
-            facebook_url,
-            twitter_url,
-            instagram_url,
-            promotion,
-            location
-            FROM musicians
-            WHERE musicians.id = ${userId}
-        `);
-
-      const { rows: instrumentsResponse } = await pg.query(sql`
-        SELECT instruments.*
-        FROM instruments
-        INNER JOIN musicians_instruments
-        ON instruments.id = musicians_instruments.instrument
-        WHERE musicians_instruments.musician= ${userId}
-    `);
-
-      const { rows: genresResponse } = await pg.query(sql`
-        SELECT genres.*
-        FROM genres
-        INNER JOIN musicians_genres
-        ON musicians_genres.genre=genres.id
-        WHERE musicians_genres.musician = ${userId}
-    `);
 
       return res.status(200).json({
         token: {
           accessToken,
           refreshToken,
         },
-        musician: {
-          ...musicianResponse,
-          genres: genresResponse,
-          instruments: instrumentsResponse,
-        },
+        musician: musicianInfo,
       });
     } catch (err) {
       return res.status(500).json({ msg: 'E_COMPARE_FAILED' });
