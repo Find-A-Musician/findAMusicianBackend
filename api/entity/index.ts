c9f1af2d65044850f8b5b266f13cf6cef363836f
RomainGuarinoni <romain.guar91@gmail.com>
2022-03-08 18:39:06 +0100

    feat(route): update get genres with typeorm

--- api/entity/index.ts
+++ api/entity/index.ts
@@ -3,3 +3,5 @@ export { Instrument } from './Instrument';
 export { Band } from './Band';
 export { Musician, Location, Promotion } from './Musician';
 export { MusicianBand, Membership } from './MusicianGroup';
+export { Token } from './Token';
+export { Event } from './Event';
